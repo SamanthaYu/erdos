9f57cc5a70c11cff85087eb9f034d4e81db97445
hyyl <hla163@sfu.ca>
2016-11-03 13:44:35 -0700

    users integrated into wrt chats/messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_many :chatrooms
+  has_many :messages
+  has_many :chatrooms, through: :messages
 end
--- db/migrate/mig_create_chatrooms.rb
+++ db/migrate/mig_create_chatrooms.rb
@@ -1,7 +1,8 @@
 class CreateChatrooms < ActiveRecord::Migration[5.0]
   def change
     create_table :chatrooms do |t|
-      t.belongs_to :user, index: true
+      t.integer :message_id
+      t.integer :user_id
       t.string :roomname
       t.timestamps
     end
