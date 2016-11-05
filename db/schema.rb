1dac5b0c8128e2b5f4e677b280fafa09f827ab79
pmiiller@sfu.ca <pmiiller@sfu.ca>
2016-11-04 17:50:02 -0700

    crude admin acounts

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20161104220940) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password"
+    t.string  "userType"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
