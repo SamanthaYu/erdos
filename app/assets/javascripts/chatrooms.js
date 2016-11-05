377a3288dc358b989b550e648fdc443316675987
irenea <irenea@sfu.ca>
2016-11-05 16:43:18 -0700

    added sliding sidebar

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -2,6 +2,30 @@ $(document).on('turbolinks:load', function() {
   submitNewMessage();
 });
 
+$(document).ready(function() {
+    $("#sidebar").hover(
+        function(){
+        $("#sidebar").css("background-color", "#55c6aa");
+    }, function() {
+        $("#sidebar").css("background-color", "#182031");
+    });
+    $("#sidebar").click(function() {
+        $("#sidebar").width(250);
+        $("#container").css({left: 250});
+    });
+    if ($("#sidebar").width() == 250) {
+        $("#sidebar").click(function() {
+            $("#sidebar").width(10);
+            $("#container").css({left: 10});
+        });
+    }
+});
+
+function closeSidebar() {
+    $("#sidebar").style.width = "10px";
+    $("#container").style.left = "10px";
+}
+
 function submitNewMessage(){
   $('textarea#message_content').keydown(function(event) {
     if (event.keyCode == 13) {
