9e3afaf9dfbcdbc504e8caa273d5ac9f2faafa5c
irenea <irenea@sfu.ca>
2016-11-05 18:03:08 -0700

    temporary button for opening sidebar

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -3,28 +3,17 @@ $(document).on('turbolinks:load', function() {
 });
 
 $(document).ready(function() {
-    $("#sidebar").hover(
-        function(){
-        $("#sidebar").css("background-color", "#55c6aa");
-    }, function() {
-        $("#sidebar").css("background-color", "#182031");
-    });
-    $("#sidebar").click(function() {
+    $("#arrow").click(function() {
         $("#sidebar").width(250);
         $("#container").css({left: 250});
+        $("#container").width($(window).width() - 250);
     });
-    if ($("#sidebar").width() == 250) {
-        $("#sidebar").click(function() {
-            $("#sidebar").width(10);
-            $("#container").css({left: 10});
-        });
-    }
+    $("#closeBtn").click(function() {
+        $("#sidebar").width(0);
+        $("#container").css({left: 0});
+        $("#container").width($(window).width());
+    });
 });
-
-function closeSidebar() {
-    $("#sidebar").style.width = "10px";
-    $("#container").style.left = "10px";
-}
 
 function submitNewMessage(){
   $('textarea#message_content').keydown(function(event) {
