f46b9b2dbd8f4e7ff58e07dc06396f2ae5d13737
irenea <irenea@sfu.ca>
2016-11-06 02:16:35 -0800

    styled sign up page, temporary

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,14 +4,4 @@ $(document).ready(function(){
         fillColor: 'ffffff',
         fillOpacity: 0.3
     });
-    $("#button2").click(function(){
-        $(".welcomePage").fadeOut("swing", function(){
-            $(".loginForm").fadeIn("swing");
-        });
-    });
-    $("#returnBtn").click(function(){
-        $(".loginForm").fadeOut("swing", function(){
-            $(".welcomePage").fadeIn("swing");
-        });
-    });
 });
