6550d34fecf58be87461e47810a006a5e3672cb9
hyyl <heather@Dagda>
2016-11-07 15:51:52 -0800

    tried passing two data params at once

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -5,6 +5,6 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
   },
 
   renderMessage: function(data) {
-    return "<p> <b>" + data.user + ": </b>" +data.message+ "</p>";
+    return "<p> <b>" + data.useridfier + ": </b>" +data.message+ "</p>";
   }
 });
