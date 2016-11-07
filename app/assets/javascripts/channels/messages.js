2c01f443ca15a87353d2a6d488722a60af78cf60
hyyl <heather@Dagda>
2016-11-07 15:29:26 -0800

    trying to bypass rendermessage function

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,10 +1,10 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
     $("#messages").removeClass('hidden')
-    return $('#messages').append(renderMessage(data));//this.renderMessage(data));
-  },
+    return $('#messages').append("<p>"+ data.message + "</p>");//this.renderMessage(data));
+  }/*
 
   renderMessage: function(data) {
     return "<p> <b>" + "Call Me Ishmael" + ": </b>" + "</p>";
-  }
+  }*/
 });
