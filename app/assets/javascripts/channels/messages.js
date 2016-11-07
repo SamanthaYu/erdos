0e5785056c8825d5be9d4d11b7d77192b76ebc3b
hyyl <heather@Dagda>
2016-11-07 15:35:01 -0800

    fixed syntax error

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -2,7 +2,7 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
     $("#messages").removeClass('hidden')
     return $('#messages').append(this.renderMessage(data));//"<p>"+ data.message + "</p>");//this.renderMessage(data));
-  }
+  },
 
   renderMessage: function(data) {
     return "<p> <b>" + "Call Me Ishmael" + ": </b>" +data.message+ "</p>";
