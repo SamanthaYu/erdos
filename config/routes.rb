91a37900cd2c2f11257ae37ce3d98d720ec9ce40
hyyl <hla163@sfu.ca>
2016-11-07 14:26:52 -0800

    fixed route logic

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,17 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get 'welcome/index'
-  resources :chatrooms
-  resources :messages
-  resources :users
-  resources :sessions
+   get '/signup', to: 'users#new'
+   post '/signup', to: 'users#create'
+   get '/login', to: 'sessions#new'
+   post '/login', to: 'sessions#create'
+   delete '/logout', to: 'sessions#destroy'
+   get 'welcome/index'
+   get '/home', to: 'welcome#show'
+   resources :chatrooms
+   resources :messages
+   resources :users
+   resources :sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
