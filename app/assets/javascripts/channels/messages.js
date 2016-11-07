addc6eee692da6983a288145b343c32e0d7e4377
hyyl <heather@Dagda>
2016-11-07 15:25:37 -0800

    forgot a +

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -5,6 +5,6 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
   },
 
   renderMessage: function(data) {
-    return "<p> <b>" + "Call Me Ishmael" + ": </b>"  "</p>";
+    return "<p> <b>" + "Call Me Ishmael" + ": </b>" + "</p>";
   }
 });
