ad74831d1952264cb4cbe34707f464a469548887
hyyl <heather@Dagda>
2016-11-07 15:24:48 -0800

    testing if data.message causes error in renderMessage

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,10 +1,10 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
     $("#messages").removeClass('hidden')
-    return $('#messages').append("<p> fire is bad</p>");//this.renderMessage(data));
+    return $('#messages').append(renderMessage(data));//this.renderMessage(data));
   },
 
   renderMessage: function(data) {
-    return "<p> <b>" + "Call Me Ishmael" + ": </b>" + data.message + "</p>";
+    return "<p> <b>" + "Call Me Ishmael" + ": </b>"  "</p>";
   }
 });
