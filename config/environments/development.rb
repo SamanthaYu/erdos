f7f3f0f154b25534b16db834ff667078fd9272f4
hyyl <heather@Dagda>
2016-11-07 14:18:25 -0800

    reset config/ to last working streaming

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false # False: For single application.js instead of 7 .js include script tags
+  config.assets.debug = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
@@ -34,4 +34,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_cable.url = "ws://localhost:5000/cable"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,10 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
 
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get 'welcome/index'
-  get '/home', to: 'welcome#show'
   resources :chatrooms
   resources :messages
   resources :users
