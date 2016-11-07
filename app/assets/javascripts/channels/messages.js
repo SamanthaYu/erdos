c684a2a8064dcbcefe84230a2a36680937b85dc6
hyyl <heather@Dagda>
2016-11-07 15:34:09 -0800

    return to original file

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,10 +1,10 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
     $("#messages").removeClass('hidden')
-    return $('#messages').append("<p>"+ data.message + "</p>");//this.renderMessage(data));
-  }/*
+    return $('#messages').append(this.renderMessage(data));//"<p>"+ data.message + "</p>");//this.renderMessage(data));
+  }
 
   renderMessage: function(data) {
-    return "<p> <b>" + "Call Me Ishmael" + ": </b>" + "</p>";
-  }*/
+    return "<p> <b>" + "Call Me Ishmael" + ": </b>" +data.message+ "</p>";
+  }
 });
