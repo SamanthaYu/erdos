c896bc9c10bdde20a44fb58efc6cd2f03e546302
pmiiller@sfu.ca <pmiiller@sfu.ca>
2016-11-06 20:22:26 -0800

    fixed logins and added chatroom creation message

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -14,6 +14,9 @@ class ChatroomsController < ApplicationController
   def create
     @chatroom = Chatroom.new(chatroom_params)
     if @chatroom.save
+      if @chatroom.roomname.blank?
+        @chatroom.roomname = chatroom_path(@chatroom)
+      end
       first = Message.create(:content => 'Chatroom \'' << @chatroom.roomname << '\' was create by ' << current_user.username, :chatroom_id => @chatroom.id, :user_id => current_user.id)
       respond_to do |format|
         format.html { redirect_to @chatroom }
