3ea91875c4fced925f381ebb14552b490e0bacac
pmiiller@sfu.ca <pmiiller@sfu.ca>
2016-11-07 19:31:50 -0800

    more fixtures

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  
+
   test "should not save messages without attributes" do
     message = Message.new
-    assert_not message.save, "Saved the chatroom without attributes"
+    assert message.save, "Saved the chatroom without attributes"
   end
 
 end
