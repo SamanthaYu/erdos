b8b7b9ccaa2374c47ad6408c1aadc48a592f4ce8
pmiiller@sfu.ca <pmiiller@sfu.ca>
2016-11-07 19:21:41 -0800

    basic tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
-    assert_response :success
+
+  test "should not save user without attributes" do
+    user = User.new
+    assert_not user.save, "Saved the user without attributes"
   end
 
 end
