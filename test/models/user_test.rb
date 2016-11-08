c355601aff8aa080147c98965c90ae2da8a6ed1d
hyyl <hla163@sfu.ca>
2016-11-07 19:59:41 -0800

    added a user too short model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:four)
+  end
+
+  test "too short username" do
+    assert @user.invalid?, "invalid too short"
+  end
+
+
+
   # test "the truth" do
   #   assert true
   # end
