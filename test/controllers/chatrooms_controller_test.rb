5856e41f175fc483c4af736dacf478495a8b60a4
pmiiller@sfu.ca <pmiiller@sfu.ca>
2016-11-07 19:21:41 -0800

    basic tests

--- test/controllers/chatrooms_controller_test.rb
+++ test/controllers/chatrooms_controller_test.rb
@@ -1,24 +1,10 @@
 require 'test_helper'
 
 class ChatroomsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get chatrooms_index_url
-    assert_response :success
-  end
 
-  test "should get new" do
-    get chatrooms_new_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get chatrooms_show_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get chatrooms_create_url
-    assert_response :success
+  test "should not save chatroom without attributes" do
+    chatroom = Chatroom.new
+    assert_not chatroom.save, "Saved the chatroom without attributes"
   end
 
 end
--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get messages_create_url
-    assert_response :success
+  
+  test "should not save messages without attributes" do
+    message = Message.new
+    assert_not message.save, "Saved the chatroom without attributes"
   end
 
 end
