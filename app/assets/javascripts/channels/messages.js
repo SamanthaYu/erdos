f48538312c9204f58625e426aaa13c591ac8dcb2
hyyl <heather@Dagda>
2016-11-10 15:36:49 -0800

    js logic for rendering

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -2,7 +2,8 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
     $("#messages").removeClass('hidden');
     $('#messages').append(this.renderMessage(data));
-    renderAllMessages();
+    var allMessages = document.getElementsByClassName('messageContent');
+    renderMath(allMessages[allMessages.length-1]);
     scrollBottom();
     return;
   },
