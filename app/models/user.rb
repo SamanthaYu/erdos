6df13149faeb168809a43218d6e10f5828f6cd2a
paul <pmiiller@sfu.ca>
2016-11-16 17:30:31 -0800

    added a few more features to friends, tables kinda look terrible though

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,22 @@ class User < ApplicationRecord
   has_many :chatrooms, through: :messages
   has_many :friendships
   has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
   before_save {username.downcase!}
+  before_destroy :delete_friendships
 
   validates :username, presence: true, length: {minimum: 3, maximum: 50}, uniqueness: {case_sensitive: false}
 
   def User.digest(string)
     cost= ActiveModel::SecurePassword.min_cost ? Bcrypt::Engine::MIN_COST : Bcrypt::Engine.cost
     Bcrypt::Password.create(string,cost:cost)
+  end
+
+  def delete_friendships
+    self.friendships.delete_all
+    self.inverse_friendships.delete_all
   end
 
   has_secure_password
