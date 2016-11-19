f64a350e6637700b43633101ebf57751805ee3c7
hyyl <hla163@sfu.ca>
2016-11-18 22:23:37 -0800

    multiple login prevention int. test

--- test/integration/loggedinredirect_test.rb
+++ test/integration/loggedinredirect_test.rb
@@ -3,11 +3,26 @@ require 'test_helper'
 class LoginRedirectTests < ActionDispatch::IntegrationTest
 
   test "can't signup while logged in" do
-    get "/chatrooms"
-    follow_redirect!
-    assert_response :success
-    assert request.path == '/'
-  end
+    visit signup_path
+    fill_in "username_area", :with => 'newuser'
+    fill_in "password_area", :with => 'trysix'
+    fill_in "password_confirmation_area", :with => 'trysix'
+    click_button('Create Account')
+    visit signup_path
+    assert page.has_no_content?('Create Account')
+    end
+
+    test "can't login again while logged in" do
+      visit signup_path
+      fill_in "username_area", :with => 'newuser'
+      fill_in "password_area", :with => 'trysix'
+      fill_in "password_confirmation_area", :with => 'trysix'
+      click_button('Create Account')
+      visit login_path
+      assert page.has_no_content?('Log in')
+      end
+
+
 
   #post "/articles",
   #  params: { article: { title: "can create", body: "article successfully." } }
