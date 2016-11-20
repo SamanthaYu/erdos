9cc33c953a122de4a00f5086b81fcf8fecb4e309
SamanthaYu <samanthayu36@gmail.com>
2016-11-19 23:59:00 -0800

    Removing repeating buttons

--- app/assets/javascripts/mathMenu.js
+++ app/assets/javascripts/mathMenu.js
@@ -1,20 +1,3 @@
-$(document).ready(function() {
-  $('.mathButton').click(function() {
-    var mathSymbol = $(this).data('val');
-    var content = $('#message_content').val();
-
-    var start = $('#message_content')[0].selectionStart;
-    var end = $('#message_content')[0].selectionEnd;
-
-    var newContent = content.substr(0, start) + mathSymbol + content.substr(end);
-    $('#message_content').val(newContent);
-
-    $('#message_content')[0].selectionStart = start + mathSymbol.length;
-    $('#message_content')[0].selectionEnd = $('#message_content')[0].selectionStart;
-    $('#message_content').focus();
-  });
-});
-
 function updateRender() {
   document.getElementById('#mathRender').value = document.getElementById('#message_content').value;
 };
