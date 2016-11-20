4b7861d595f26552cb82447fc39fec7612e79df7
SamanthaYu <samanthayu36@gmail.com>
2016-11-19 23:07:41 -0800

    Merging with old math

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,31 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require_tree ./channels
 //= require math
+//= require mathMenu
 //= require chatrooms
 //= require welcome
 //= require_tree .
+
+document.addEventListener("turbolinks:load", function() {
+    renderAllMessages();
+    renderAllUsernames();
+    renderAllButtons();
+});
+
+function openSidebar(){
+    $("#sideBarOpener").hide();
+    $("#sidebar").width(250);
+    $("#container").css({left: 250});
+    $("#container").width($(window).width() - 250);
+};
+function closeSidebar() {
+    $("#sidebar").width(0);
+    $("#container").css({left: 0});
+    $("#container").width($(window).width());
+    setTimeout(function() {
+        $("#sideBarOpener").show();
+    }, 500);
+};
