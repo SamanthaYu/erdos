3cc18227a235b5e0836ee5012cb1d267a5d2b399
SamanthaYu <samanthayu36@gmail.com>
2016-11-19 22:42:03 -0800

    Turbolinks GAHgit add .! Rendering works. Can't tell about messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,30 +11,11 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree ./channels
-//= require chatrooms
 //= require math
+//= require chatrooms
 //= require welcome
 //= require_tree .
-
-$(document).on('turbolinks:load', function(){
-    renderAllMessages();
-    renderAllUsernames();
-});
-
-function openSidebar(){
-    $("#sideBarOpener").hide();
-    $("#sidebar").width(250);
-    $("#container").css({left: 250});
-    $("#container").width($(window).width() - 250);
-};
-function closeSidebar() {
-    $("#sidebar").width(0);
-    $("#container").css({left: 0});
-    $("#container").width($(window).width());
-    setTimeout(function() {
-        $("#sideBarOpener").show();
-    }, 500);
-};
