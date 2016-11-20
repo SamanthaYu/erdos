470e7bf969a6cc62765b02da32c095aff9401e30
irenea <irenea@sfu.ca>
2016-11-20 01:38:47 -0800

    changed minor details

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -3,13 +3,13 @@ $(document).on('turbolinks:load', function() {
   scrollBottom();
   renderLastMessage();
   renderLastUsername();
-});
+})
 
 function submitNewMessage(){
   $('textarea#message_content').keydown(function(event) {
     if (event.keyCode == 13) {
         $('[data-send="message"]').click();
-        $('[data-textarea="message"]').val(" ")
+        $('[data-textarea="message"]').val("")
         return false;
      }
   });
