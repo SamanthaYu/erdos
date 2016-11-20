df52019ab95e4f7c311bd627f7f512c1deb82b71
irenea <irenea@sfu.ca>
2016-11-20 15:44:06 -0800

    try to fix multiple rendered messages

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -10,17 +10,17 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
 
   renderMessage: function(data) {
     var retmess='';
-    var timestamp='<span class="timeStamp" id="currentUserTime">' + data.timestamp + '</span>';
+    var timestamp=data.timestamp;
     var avatarlink='<span class="userAvatar"><img src="'+data.avatarurl+'"></span>'
     if (data.currentuser==data.poster){
       retmess+='<div id="currentUserMessage" class="message">';
-      retmess+='<div class="messageHeader"><p>' + timestamp +'<span class="username"><strong>'+data.poster+'</strong></span>';
-      retmess+=avatarlink + '</p>';
+      retmess+='<div class="messageHeader"><p><span class="timeStamp" id="currentUserTime">' + timestamp + '</span><span class="username"><strong>'+data.poster+'</strong></span>';
+      retmess+=avatarlink +'</p>';
     }
     if (data.currentuser!=data.poster){
       retmess+='<div id="otherUserMessage" class="message">';
       retmess+='<div class="messageHeader"><p>' + avatarlink +'<span class="username"><strong>'+data.poster+'</strong></span>';
-      retmess+=timestamp + '</p>';
+      retmess+='<span class="timeStamp">' + timestamp + '</span></p>';
     }
 
     retmess+='</div><div class="messageContent">'+data.message + '</div></div>';
