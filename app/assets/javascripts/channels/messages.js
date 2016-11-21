d2e99a3c6b86b3983bb0ec3cc89f7baf51b52445
irenea <irenea@sfu.ca>
2016-11-20 21:55:53 -0800

    fix not rendering avatar images

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -11,15 +11,16 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
   renderMessage: function(data) {
     var retmess='';
     var timestamp=data.timestamp;
-    //var avatarlink='<span class="userAvatar"><img src="'+data.avatarurl+'"></span>'
+    var avatarlink1='<span class="userAvatar" id="currentUserAvatar"><img src="'+data.avatarurl+'"></span>';
+    var avatarlink2='<span class="userAvatar" id="otherUserAvatar"><img src="'+data.avatarurl+'"></span>';
     if (data.currentuser==data.poster){
       retmess+='<div id="currentUserMessage" class="message">';
       retmess+='<div class="messageHeader"><p><span class="timeStamp" id="currentUserTime">' + timestamp + '</span><span class="username"><strong>'+data.poster+'</strong></span>';
-      retmess+='<span class="userAvatar" id="currentUserAvatar"><img src="'+data.avatarurl+'"></span></p>';
+      retmess+=avatarlink1+'</p>';
     }
     if (data.currentuser!=data.poster){
       retmess+='<div id="otherUserMessage" class="message">';
-      retmess+='<div class="messageHeader"><p><span class="userAvatar" id="otherUserAvatar"><img src="'+data.avatarurl+'"></span><span class="username"><strong>'+data.poster+'</strong></span>';
+      retmess+='<div class="messageHeader"><p>'+ avatarlink2 +'<span class="username"><strong>'+data.poster+'</strong></span>';
       retmess+='<span class="timeStamp">' + timestamp + '</span></p>';
     }
 
