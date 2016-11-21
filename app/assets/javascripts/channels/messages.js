dcdfd445bbbd99d087412ac8fbd2a4aeb80e037b
irenea <irenea@sfu.ca>
2016-11-21 10:50:43 -0800

    fixed error messages not rendering for guest login

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,6 +1,6 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
-    if ($('h1').text()==data.chatroomname){
+    if ($('h1').text()===data.chatroomname){
       $("#messages").removeClass('hidden');
       $('#messages').append(this.renderMessage(data));
       scrollBottom();
@@ -15,15 +15,15 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
     var timestamp=data.timestamp;//LocalTime.relativeTimeAgo(new Date(data.timestamp));
     var avatarlink1='<span class="userAvatar" id="currentUserAvatar"><img src="'+data.avatarurl+'"></span>';
     var avatarlink2='<span class="userAvatar" id="otherUserAvatar"><img src="'+data.avatarurl+'"></span>';
-    if (data.currentuser==data.poster){
+    if (data.currentuser === data.poster){
       retmess+='<div id="currentUserMessage" class="message">';
-      retmess+='<div class="messageHeader"><p><span class="timeStamp" id="currentUserTime">' + '<%=local_time_ago(timestamp)%>' + '</span><span class="username"><strong>'+data.poster+'</strong></span>';
+      retmess+='<div class="messageHeader"><p><span class="timeStamp" id="currentUserTime">' + timestamp + '</span><span class="username"><strong>'+data.poster+'</strong></span>';
       retmess+=avatarlink1+'</p>';
     }
     else {
       retmess+='<div id="otherUserMessage" class="message">';
       retmess+='<div class="messageHeader"><p>'+ avatarlink2 +'<span class="username"><strong>'+data.poster+'</strong></span>';
-      retmess+='<span class="timeStamp">' + '<%=local_time_ago(timestamp)%>' + '</span></p>';
+      retmess+='<span class="timeStamp">' + timestamp + '</span></p>';
     }
 
     retmess+='</div><div class="messageContent">'+data.message + '</div></div>';
