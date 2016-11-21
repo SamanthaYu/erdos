39a4f7342e325212125ac25098034cf44a068d61
SamanthaYu <samanthayu36@gmail.com>
2016-11-21 00:18:57 -0800

    Few changes to math stuff before getting master

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,21 +26,6 @@ document.addEventListener("turbolinks:load", function() {
     renderAllMessages();
     renderAllUsernames();
     renderAllButtons();
-
-    $('.mathButton').click(function() {
-      var mathSymbol = $(this).data('val');
-      var content = $('#message_content').val();
-
-      var start = $('#message_content')[0].selectionStart;
-      var end = $('#message_content')[0].selectionEnd;
-
-      var newContent = content.substr(0, start) + mathSymbol + content.substr(end);
-      $('#message_content').val(newContent);
-
-      $('#message_content')[0].selectionStart = start + mathSymbol.length;
-      $('#message_content')[0].selectionEnd = $('#message_content')[0].selectionStart;
-      $('#message_content').focus();
-    });
 });
 
 function openSidebar(){
--- app/assets/javascripts/mathMenu.js
+++ app/assets/javascripts/mathMenu.js
@@ -1,3 +1,23 @@
+document.addEventListener("turbolinks:load", function() {
+  $('.mathButton').click(function() {
+    var mathSymbol = $(this).data('val');
+    var content = $('#message_content').val();
+
+    var start = $('#message_content')[0].selectionStart;
+    var end = $('#message_content')[0].selectionEnd;
+
+    var beforeInsert = content.substr(0, start);
+    var afterInsert = content.substr(end);
+
+    var newContent = beforeInsert + mathSymbol + afterInsert;
+    $('#message_content').val(newContent);
+
+    $('#message_content')[0].selectionStart = start + mathSymbol.length;
+    $('#message_content')[0].selectionEnd = $('#message_content')[0].selectionStart;
+    $('#message_content').focus();
+  });
+});
+
 function updateRender() {
   document.getElementById('#mathRender').value = document.getElementById('#message_content').value;
 };
