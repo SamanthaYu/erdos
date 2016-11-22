1d972dbae0192355be6e8dead4d4af649af02a9d
SamanthaYu <samanthayu36@gmail.com>
2016-11-21 19:05:47 -0800

    Changes from oldmaster

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,42 +1,65 @@
-class User < ApplicationRecord
-  mount_uploader :avatar, AvatarUploader
-  has_many :messages
-  has_many :chatrooms, through: :messages
-  has_many :private_chats
-  has_many :private_chatrooms, :through => :private_chats, :source => :chatroom
-  has_many :friendships
-  has_many :friends, :through => :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
-  validates_processing_of :avatar
-  validate :avatar_size_validation
-  attr_accessor :skip_password_validation
+class UsersController < ApplicationController
+    layout 'signup', only: [:new, :create]
 
-  before_save {username.downcase!}
-  before_destroy :delete_friendships
-
-  validates :username, presence: true, length: {minimum: 3, maximum: 50}, uniqueness: {case_sensitive: false}
-
-  def User.digest(string)
-    cost= ActiveModel::SecurePassword.min_cost ? Bcrypt::Engine::MIN_COST : Bcrypt::Engine.cost
-    Bcrypt::Password.create(string,cost:cost)
+  def index
+    @user=User.new
+    @users=User.all
   end
 
-  def delete_friendships
-    self.friendships.delete_all
-    self.inverse_friendships.delete_all
+  def new
+    loggedinredirect
+    @user = User.new
   end
 
-  def avatar_size_validation
-    errors[:avatar] << "too big - avatar should be less than 1MB" if avatar.size > 1.0.megabytes
+  def edit
+    @user = User.find(params[:id])
   end
 
-  def allChatrooms
-      self.chatrooms |= self.private_chatrooms
-      self.chatrooms.sort_by(&:created_at)
+  def create
+    loggedinredirect
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      if (@user.userType == "Guest")
+        redirect_to chatrooms_url
+      else
+        redirect_to @user
+      end
+    else
+      render :new
+    end
   end
 
-  has_secure_password
-  validates :password, presence: true, length: {minimum: 6, maximum: 30}, unless: :skip_password_validation
+  def update
+    @user=User.find(params[:id])
+    @user.skip_password_validation=true
+    respond_to do |format|
+      if @user.update(params.require(:user).permit(:avatar,:remove_avatar))
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.present?
+      @user.destroy
+    end
+    redirect_to users_url
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation, :userType,:avatar)
+    end
 
 end
