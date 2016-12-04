b02e0177a3509a68c6419cbdf7cf9540c408f1b5
hyyl <hla163@sfu.ca>
2016-12-04 01:04:10 -0800

    added all emojis to emojis css class

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -36,7 +36,7 @@ module SessionsHelper
 
   def emojime(content)
     EmojiParser.parse(content) do |emoji|
-      '<img src="/'+emoji.image_filename+'" alt=":'+emoji.name+':">'
+      '<img src="/'+emoji.image_filename+'" alt=":'+emoji.name+':" class="emojis">'
     end
   end
 end
