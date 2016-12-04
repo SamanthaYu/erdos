68d2fa9d56f45d3b32850e69341171d32a90d7ec
SamanthaYu <dschandl@sfu.ca>
2016-12-03 22:19:50 -0800

    Message box retraction

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,3 +1,18 @@
 $(window).load(function() {
   $("#messageContainer").animate({ scrollTop: $('#messageContainer')[0].scrollHeight});
 });
+
+function openMessageBox(){
+    $("#messageBoxOpener").hide();
+    $("#inputForm").height(200);
+    setTimeout(function() {
+      $("#messageContainer").css({bottom: 200});
+    }, 500);
+};
+function closeMessageBox() {
+    $("#inputForm").height(0);
+    $("#messageContainer").css({bottom: 0});
+    setTimeout(function() {
+        $("#messageBoxOpener").show();
+    }, 500);
+};
