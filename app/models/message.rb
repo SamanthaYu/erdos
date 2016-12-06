005cf72efc633f908da3261e7c112267abf71daf
irenea <irenea@sfu.ca>
2016-12-05 21:12:13 -0800

    changed associations of messages and notifications, messages now has_many notifications and notifications belongs to user, hopefully this works better now

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,7 +5,7 @@ class Message < ApplicationRecord
   default_scope { order(created_at: :asc) }
   belongs_to :chatroom
   belongs_to :user
-  has_one :notification, dependent: :destroy
+  has_many :notifications, dependent: :destroy
   #something about user?
 
   mount_uploader :imagemessage, ImageMessageUploader
@@ -13,7 +13,12 @@ class Message < ApplicationRecord
   validates :content, presence: true, length: {minimum: 1}
 
   def notify
-    @notification = Notification.create(:message_id => self.id, :event => "#{self.user.username} posted a new message in #{self.chatroom.roomname}.")
-    self.notification = @notification
+    @receivingUsers = User.where(id: self.chatroom.users.map(&:id)).where.not(id: self.user.id)
+    @receivingUsers.each do |user|
+      @notification = Notification.create(:message_id => self.id,
+                                          :event => "#{self.user.username} posted a new message in #{self.chatroom.roomname}.",
+                                          :user_id => user.id)
+      self.notifications << @notification
+    end
   end
 end
