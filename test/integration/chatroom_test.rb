52f2edc9b177ba217068184c344d51af7c41f7bd
hyyl <hla163@sfu.ca>
2016-12-06 12:28:31 -0800

    fix h1 content so messages render correctly in autonamed chatrooms

--- test/integration/chatroom_test.rb
+++ test/integration/chatroom_test.rb
@@ -37,20 +37,5 @@ class ChatroomTests < ActionDispatch::IntegrationTest
     assert page.has_content?("Chatroom 'TestName' was created by newuser")
   end
 
-  test "can post inside the chatroom" do
-    visit signup_path
-    fill_in "username_area", :with => 'newuser'
-    fill_in "password_area", :with => 'trysix'
-    fill_in "password_confirmation_area", :with => 'trysix'
-    click_button('Create Account')
-    visit chatrooms_path
-    fill_in "chatroom[roomname]",   :with => 'TestName'
-    click_button('Create Chatroom')
-    savedurl=current_url
-    fill_in('message_content', :with => 'This is a message')
-    click_on('message_submit')
-    visit savedurl
-    assert page.has_content?('This is a message')
-  end
 
 end
---
+++ test/integration/message_test.rb
@@ -0,0 +1,98 @@
+require 'test_helper'
+
+class UserTests < ActionDispatch::IntegrationTest
+
+
+    test "mesasges can be posted" do
+      visit signup_path
+      fill_in "username_area", :with => 'newuser'
+      fill_in "password_area", :with => 'trysix'
+      fill_in "password_confirmation_area", :with => 'trysix'
+      click_button('Create Account')
+      visit chatrooms_path
+      fill_in "chatroom[roomname]",   :with => 'TestName'
+      click_button('Create Chatroom')
+      savedurl=current_url
+      fill_in('message_content', :with => 'This is a message')
+      click_on('message_submit')
+      visit savedurl
+      assert page.has_content?('This is a message')
+    end
+
+  test "non-image messages can be edited" do
+    visit signup_path
+    fill_in "username_area", :with => 'newuser'
+    fill_in "password_area", :with => 'trysix'
+    fill_in "password_confirmation_area", :with => 'trysix'
+    click_button('Create Account')
+    visit chatrooms_path
+    fill_in "chatroom[roomname]",   :with => 'TestName'
+    click_button('Create Chatroom')
+    savedurl=current_url
+    fill_in('message_content', :with => 'This is a message')
+    click_on('message_submit')
+    visit savedurl
+    assert page.has_content?('This is a message')
+  end
+
+=begin
+  test "can signup as an admin user" do
+    visit signup_path
+    fill_in "username_area", :with => 'newuser'
+    fill_in "password_area", :with => 'trysix'
+    fill_in "password_confirmation_area", :with => 'trysix'
+    select "Admin", :from => 'user[userType]'
+    click_button('Create Account')
+    assert page.has_content?('Admin')
+  end
+=end
+
+  test "can enter site as a guest user" do
+    visit signup_path
+    click_button('Guest Login')
+    fill_in "guest_username_area", :with => 'guestuser'
+    click_button('Login as Guest')
+    visit chatrooms_path
+    assert page.has_content?('List of Current Chatrooms')
+  end
+
+  test "non-guest users can log into their accounts again" do
+    visit signup_path
+    fill_in "username_area", :with => 'tologout'
+    fill_in "password_area", :with => 'trysix'
+    fill_in "password_confirmation_area", :with => 'trysix'
+    click_button('Create Account')
+    click_on 'Log out'
+    visit login_path
+    fill_in "username_area", :with => 'tologout'
+    fill_in "password_area", :with => 'trysix'
+    click_button('Log in')
+    assert page.has_content?('tologout')
+  end
+
+
+  test "admins can decide to delete chatrooms" do
+    visit signup_path
+    fill_in "username_area", :with => 'newadmin'
+    fill_in "password_area", :with => 'trysix'
+    fill_in "password_confirmation_area", :with => 'trysix'
+    click_button('Create Account')
+    user=User.find_by(username: 'newadmin')
+    user.update_attribute :userType, 'Admin'
+    visit chatrooms_path
+    assert page.has_content?('Destroy Chatroom')
+  end
+
+  test "admins can decide to delete users" do
+    visit signup_path
+    fill_in "username_area", :with => 'newadmin'
+    fill_in "password_area", :with => 'trysix'
+    fill_in "password_confirmation_area", :with => 'trysix'
+    click_button('Create Account')
+    user=User.find_by(username: 'newadmin')
+    user.update_attribute :userType, 'Admin'
+    visit users_path
+    assert page.has_content?('Destroy')
+  end
+
+end
