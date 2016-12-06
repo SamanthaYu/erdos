44c8a4e3ecc1327f7ce55d2847150d40200fb2eb
irenea <irenea@sfu.ca>
2016-12-06 14:26:31 -0800

    added test for delete friendships method and case sensitivity for usernames

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
   setup do
     @user = users(:four)
     @user2 = users(:six)
@@ -24,7 +28,29 @@ class UserTest < ActiveSupport::TestCase
     assert @user4.invalid?, "password is too long"
   end
 
-  # test "the truth" do
-  #   assert true
-  # end
+  test "usernames are case sensitive" do
+    assert_equal("MaryPoppins", @user4.username)
+    assert_not_equal("marypoppins", @user4.username)
+  end
+
+  test "delete friendships should delete all friendships" do
+    @friend1 = users(:receiver1)
+    @friend2 = users(:receiver2)
+    @friend3 = users(:receiver3)
+
+    @friendship1 = friendships(:three)
+    @friendship2 = friendships(:four)
+
+    # friend1 friends with friend2 and friend3
+    assert_equal(2, @friend1.friendships.count)
+
+    # friend3 is not a friend of friend1 (friend 3 is inverse friend)
+    assert_equal(1, @friend3.inverse_friendships.count)
+
+    @friend1.delete_friendships
+    assert_equal(0, @friend1.friendships.count)
+
+    @friend3.delete_friendships
+    assert_equal(0, @friend3.inverse_friendships.count)
+  end
 end
